ef1cc19a2b36bcee87b3d5b9078342877b780168
martijn <mawegink@gmail.com>
2021-05-25 16:32:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ client.on("ready", async () => {
       dashboard = new botdash.APIclient(process.env.botdash);
       //dashboard = new botdash.APIclient(botConfig.botdash);
       
-      console.log(discord.Channel.arguments);
+      
+      console.log(discord.GuildChannel.names);
 
 });
 
