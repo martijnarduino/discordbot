8a4e125380a07bafd14328bb8e01075a456a33de
martijn <mawegink@gmail.com>
2021-05-25 16:41:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ if (command === `${freeprefix}avatar`){
 
 if(command === `${test}reload`){
         
-    console.log(discord.GuildMember.prototype);
+    console.log(discord.Guild.prototype.members);
     return message.channel.send("Hallo!"); 
 }
 }); 
